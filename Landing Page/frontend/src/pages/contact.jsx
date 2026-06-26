0307569112c38abfaa32ee523da38c52ad302d49
inessalazar27 <programacionines27@gmail.com>
2026-06-25 22:34:08 -0400

    elimnado

--- Landing Page/frontend/src/pages/contact.jsx
+++
@@ -1,9 +0,0 @@
-const contact = () => {
-  return (
-    <div style={{ padding: '50px', color: 'white' }}>
-      <h1>Contactos</h1>
-      <p>Esta es una página en blanco de prueba.</p>
-    </div>
-  );
-};
-export default contact;
